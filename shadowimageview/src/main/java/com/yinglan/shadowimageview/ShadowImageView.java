35586373ee326ab240341f70e2b147af4c953f7a
yinglan <sufly0001@gmail.com>
2017-04-28 10:43:25 +0800

    Change

--- shadowimageview/src/main/java/com/yinglan/shadowimageview/ShadowImageView.java
+++ shadowimageview/src/main/java/com/yinglan/shadowimageview/ShadowImageView.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2016 yinglan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.yinglan.shadowimageview;
 
 import android.content.Context;
@@ -18,6 +33,16 @@ import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
+
+/**
+ * ================================================
+ * 作    者：sufly0001@gmail.com
+ * 版    本：1.1
+ * 创建日期 ：2017/04/01
+ * 描    述：
+ * 修订历史： 修改显示
+ * ================================================
+ */
 public class ShadowImageView extends RelativeLayout {
 
     private int shadowRound = 0;
